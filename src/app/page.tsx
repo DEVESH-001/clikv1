42ff8c8f399b63cc0e35c03ceec561355075f14e
Devesh Yadav <dd4168@srmist.edu.in>
2024-12-11 20:40:32 +0530

    v1_till_faqs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import Testimonials from "@/components/AnimatedTestimonials";
 import FooterCta from "@/components/Cta_footer";
 import FaqsSection from "@/components/FaqsSection";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Pricing from "@/components/Pricing";
 import Problem from "@/components/Problem";
-// import FocusCardsDemo from "@/components/Testimonials";
 import WorkingSection from "@/components/WorkingSection";
-import WhyClikAi from "@/components/WhyClikAi";
+
 import React from "react";
 
 const page = () => {
@@ -16,14 +14,10 @@ const page = () => {
       <HeroSection />
       <Problem />
       <WorkingSection />
-      {/* <FocusCardsDemo/> */}
-      <Testimonials/>
-      <WhyClikAi/>
 
       <Pricing />
       <FaqsSection />
       <FooterCta />
-
       <Footer />
     </main>
   );
